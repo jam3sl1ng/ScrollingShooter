2ba780cd05cd58b485e1070fc79b075dc5f9a30b
jam3sl1ng <jamesling596@gmail.com>
2024-12-09 17:47:29 +0000

    V1.0

---
+++ main.py
@@ -0,0 +1,36 @@
+import pygame
+
+pygame.init()
+
+# Global consts
+SCREEN_WIDTH = 800
+SCREEN_HEIGHT = int(SCREEN_WIDTH * 0.8)
+
+screen = pygame.display.set_mode((SCREEN_WIDTH, SCREEN_HEIGHT))
+pygame.display.set_caption('Scrolling Shooter')
+
+class Soldier(pygame.sprite.Sprite):
+    def __init__(self, x, y, scale):
+        pygame.sprite.Sprite.__init__(self)
+        img = pygame.image.load('img/player/Idle/0.png')
+        self.image = pygame.transform.scale(img, (int(img.get_width() * scale), int(img.get_height() * scale)))
+        self.rect = self.image.get_rect()
+        self.rect.center = (x, y)
+    
+    def draw(self):
+        screen.blit(self.image, self.rect)
+
+player = Soldier(200, 200, 3)
+
+run = True
+while run:
+
+    player.draw()
+
+    for event in pygame.event.get():
+        if event.type == pygame.QUIT: # Quit game
+            run = False
+    
+    pygame.display.update()
+
+pygame.quit()
